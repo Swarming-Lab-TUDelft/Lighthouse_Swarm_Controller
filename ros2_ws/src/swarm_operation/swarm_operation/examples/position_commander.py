3e51f3145e4f4b75e1fa8514e098f459409fd240
SeppeVdB <79260534+SeppeVdB@users.noreply.github.com>
2023-06-29 14:32:22 +0200

    added position commander example

--- ros2_ws/src/swarm_operation/swarm_operation/examples/position_commander.py
+++ ros2_ws/src/swarm_operation/swarm_operation/examples/position_commander.py
@@ -1,12 +1,16 @@
-import sys
 import rclpy
 from rclpy.node import Node
-from rclpy.qos import QoSProfile, QoSDurabilityPolicy
-from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+
+from std_msgs.msg import String
 
 from helper_classes import SwarmController
 
 import math
+import sys
+
+"""
+In this example, the drones will fly in a grid pattern using position commands.
+"""
 
 
 class PositionCommanderExample(Node):
@@ -14,14 +18,30 @@ class PositionCommanderExample(Node):
         super().__init__("position_commander",
                          allow_undeclared_parameters=False,
                          automatically_declare_parameters_from_overrides=True)
-
+        
         self.num_radios = self.get_parameter('number_radios').value
 
+        # create Gui command callback to terminate this node when the GUI closes
+        self.GUI_command_sub = self.create_subscription(String, 'GUI_command', self.GUI_command_callback, 10)
+
+        # create swarm controller
         self.controller = SwarmController(self, self.num_radios)
 
+        # start main loop timer at 10 Hz
         self.main_loop_timer = self.create_timer(0.1, self.main_loop_cb)
     
+    def GUI_command_callback(self, msg):
+        """
+        Terminate this node when the GUI sends a terminate command (closing the GUI).
+        """
+        if msg.data == "terminate/kill all":
+            self.destroy_node()
+            sys.exit()
+
     def main_loop_cb(self):
+        """
+        Main control loop.
+        """
         no_drones = len(self.controller.get_swarming_uris())
         if no_drones > 0:
             grid_points = self.create_grid(no_drones)
